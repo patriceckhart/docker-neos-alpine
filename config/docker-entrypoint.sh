042377671af51df1f22ddd0d5d74f2941a6605e2
Patric Eckhart <mail@patriceckhart.com>
2020-02-03 14:23:11 +0100

    no message

--- config/docker-entrypoint.sh
+++ config/docker-entrypoint.sh
@@ -3,10 +3,9 @@ set -ex
 
 DIR="NeosCMS-Boilerplate"
 if [ -d "$DIR" ]; then
-  
-  #echo "NeosCMS-Boilerplate exists."
+  echo "NeosCMS-Boilerplate exists."
 else
-  #echo "Downloading Neos"
+  echo "Downloading Neos"
   #git clone https://github.com/patriceckhart/NeosCMS-Boilerplate.git
   #mv NeosCMS-Boilerplate/composer.json /data/neos/composer.json
   #mv NeosCMS-Boilerplate/Source/* /data/neos/Source/
