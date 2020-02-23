fb065afd3d9a1c6b7017dbbd57ff84558f2c593b
Patric Eckhart <mail@patriceckhart.com>
2020-02-23 18:07:51 +0100

    Neos update process modified

--- config/neos/update-neos.sh
+++ config/neos/update-neos.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "update" > /data/neos/Web/update.txt
+
 cd /data/neos && composer update --no-interaction
 
 cd /data/neos && ./flow flow:core:setfilepermissions
 
 cd /data/neos && FLOW_CONTEXT=Production ./flow flow:cache:flush --force
+
+rm -rf /data/neos/Web/update.txt
