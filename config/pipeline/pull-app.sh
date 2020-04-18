c1fed70fa2dac3696a9d3202e1649a8a6749b019
Patric Eckhart <mail@patriceckhart.com>
2020-04-18 15:40:40 +0200

    composer update routine added

--- config/pipeline/pull-app.sh
+++ config/pipeline/pull-app.sh
@@ -8,6 +8,14 @@ else
 	cd /data/neos && git pull https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/$GITHUB_USERNAME/$GITHUB_REPOSITORY
 fi
 
+cd /data/neos && composer update --no-interaction
+
+cd /data/neos && ./flow flow:core:setfilepermissions
+
+cd /data/neos && FLOW_CONTEXT=Production ./flow flow:cache:flush --force
+
+cd /data/neos && ./flow flow:cache:flush --force
+
 chown -R www-data:www-data /data/neos
 
 echo "Git repository was pulled successfully."
