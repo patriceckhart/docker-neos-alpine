4c4f80fc3f4ce7d807886e8133f7c6948418c52b
Patric Eckhart <mail@patriceckhart.com>
2020-05-12 18:29:33 +0200

    package rescan and db update added in pullapp and updateneos command

--- config/pipeline/pull-app.sh
+++ config/pipeline/pull-app.sh
@@ -14,6 +14,10 @@ cd /data/neos && composer update --no-interaction
 
 cd /data/neos && ./flow flow:core:setfilepermissions
 
+cd /data/neos && ./flow flow:package:rescan
+
+cd /data/neos && ./flow doctrine:update
+
 cd /data/neos && FLOW_CONTEXT=Production ./flow flow:cache:flush --force
 
 cd /data/neos && ./flow flow:cache:flush --force
