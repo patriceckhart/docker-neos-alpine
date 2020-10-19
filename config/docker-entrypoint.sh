bbfd8abd49b26e5a77f797f5e02de726437d9176
Patric Eckhart <mail@patriceckhart.com>
2020-10-19 14:31:45 +0200

    + cron directory creation if these do not exist

--- config/docker-entrypoint.sh
+++ config/docker-entrypoint.sh
@@ -138,22 +138,53 @@ CRONDIR="/data/cron/"
 
 if [ -d "$CRONDIR" ]; then
   
-  echo "Cron directories exist."
+  echo "Cron directory exist."
 
 else
 
-  echo "Create cron directories ..."
+  echo "Create cron directory ..."
 
   mkdir -p /data/cron
+
+  echo "Cron directory created."
+
+fi
+
+if [ -d "/data/cron/1min" ]; then
+  echo "Cron 1 min directory exist."
+else
   mkdir -p /data/cron/1min
+  echo "Cron 1 min directory created."
+fi
+if [ -d "/data/cron/15min" ]; then
+  echo "Cron 15 min directory exist."
+else
   mkdir -p /data/cron/15min
+  echo "Cron 15 min directory created."
+fi
+if [ -d "/data/cron/hourly" ]; then
+  echo "Cron hourly directory exist."
+else
   mkdir -p /data/cron/hourly
+  echo "Cron hourly directory created."
+fi
+if [ -d "/data/cron/daily" ]; then
+  echo "Cron daily directory exist."
+else
   mkdir -p /data/cron/daily
+  echo "Cron daily directory created."
+fi
+if [ -d "/data/cron/weekly" ]; then
+  echo "Cron weekly directory exist."
+else
   mkdir -p /data/cron/weekly
+  echo "Cron weekly directory created."
+fi
+if [ -d "/data/cron/monthly" ]; then
+  echo "Cron monthly directory exist."
+else
   mkdir -p /data/cron/monthly
-
-  echo "Cron directories created."
-
+  echo "Cron monthly directory created."
 fi
 
 rm -rf /etc/periodic/1min
